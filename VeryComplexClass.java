4724d4f20e7692c523b43a162e9e00606d86b89c
Tat <readmepls@mail.ru>
2020-06-14 16:47:56 +0300

    first commit

---
+++ VeryComplexClass.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task38.task3802;
+
+/* 
+Проверяемые исключения (checked exception)
+*/
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws IOException {
+
+       File file = new File("e:\\thisfileisnotexist.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+
+    }
+
+    public static void main(String[] args) {
+        VeryComplexClass veryComplexClass = new VeryComplexClass();
+        try {
+            veryComplexClass.veryComplexMethod();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.out.println("ioexception");
+
+        }
+        catch(Exception e ){
+            System.out.println("exception");
+        }
+        finally{
+            System.out.println("finally");
+        }
+
+
+
+    }
+}
